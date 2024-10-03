755349d1c049617c7a7c7a49b8d42cafe16e5cac
Jaycee-Zh <jaycee.zh@yahoo.com>
2024-10-03 15:55:06 -0400

    Update sketch.js
    - add [optional] mark

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ Stories and Rules：
 - （ a user pick a costume, scan the QR code to open this APP, grants the access("adopt me") and turn on the speaker, and install their phone )
 - ( they need to stand at certain marked location and find/wait for another user to stand at the related location )
 - users' goal is to reach certain points within given time (e.g. 5 mins)
-- they shake their phone to send out a pitch
+- [optional] they shake their phone to send out a pitch
 - another user's phone recognize the pitch and start a "conversation"
 - when the conversation ends, update the points
-- humans, who don't have the costume for their phone, should try to be taken photo by mimic the pitch of the phone
+- [optional; test before decided] humans, who don't have the costume for their phone, should try to be taken photo by mimic the pitch of the phone
 - users will be awarded or punished based on the human's behavior ( a riskier choice )
 - By the end, if no one can reach the goal in time, who has the highest pointing win.
 - (rewards: some decorations)
