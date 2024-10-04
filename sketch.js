54c2bfeb7fe28d642a1bfd3dcde4b6a8d6640af1
J.C. Zhang <jaycee.zh@yahoo.com>
2024-10-03 23:07:18 -0400

    Test file structure
    - generated 12 html
    - to test how to use one sketch "generate” 12 pets (for the test, only 1 and 2 have conf, add others later)

--- sketch.js
+++ sketch.js
@@ -22,10 +22,16 @@ Stories and Rules：
 
 function setup() {
   createCanvas(400, 400);
+  
 }
 
 function draw() {
   background(220);
+  // for test file structure; delete later
+  text(petName, 60, 60); 
+  fill(eyeShadeColour);
+  circle(120,120,60)
+
 }
 
 /*
