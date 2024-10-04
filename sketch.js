0d517a5199ca97f562bbebfd1b7ca89a78adf1ee
J.C. Zhang <jaycee.zh@yahoo.com>
2024-10-03 23:21:30 -0400

    fix deploy

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(400, 400);
+    
+  }
+  
+  function draw() {
+    background(220);
+    // for test file structure; delete later
+    text(petName, 60, 60); 
+    fill(eyeShadeColour);
+    circle(120,120,60)
+  
+  }
