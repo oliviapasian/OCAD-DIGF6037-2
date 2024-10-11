042425fd54ebdfdcb20a9b5c099b8947450674a8
H Z <Hzhao@ocadu.ca>
2024-10-11 10:13:32 -0400

    Working

--- sketch.js
+++ sketch.js
@@ -25,9 +25,21 @@ DEVICE Gyroscope by remarkability https://editor.p5js.org/remarkability/sketches
 
 let betweenConversation = 5000; 
 
+
+let byebye = []
+function sayBye()
+{
+  let temp = random(byebye)
+  temp.play()
+}
+
 function preload() {
    heart = loadImage('./assets/heart.gif');
    heartGoal = loadImage('./assets/heart-goal.gif');
+   byebye[0] = loadSound('assets/byebye1.mp3');
+   byebye[1] = loadSound('./assets/byebye2.mp3');
+   byebye[2] = loadSound('./assets/byebye3.mp3');
+
 }
 
 function setup() {
@@ -141,6 +153,7 @@ function resetPet()
   convoDuration = 0;
   lastUnstableTime = millis()
   loopStartTime = millis()
+  sayBye()
 }
 
 function draw() {
